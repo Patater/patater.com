dd2878cd079fe2b00f2c605cb52ad29555486b64
Jaeden Amero <jaeden@patater.com>
2021-04-02 11:54:35 +0100

    bastille: WIP

--- patater.com/setup-bastille.sh
+++ patater.com/setup-bastille.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+mkdir -p /usr/local/bastille
+chmod 0750 /usr/local/bastille
+zfs create zroot/bastille
+
 # Use bastille to set up jails
 bastille bootstrap 12.2-RELEASE
-bastille create www 12.2-RELEASE 10.0.0.2
+bastille create www 12.2-RELEASE 10.0.0.3
+bastille create sftp 12.2-RELEASE 10.0.0.4
+bastille create irc 12.2-RELEASE 10.0.0.5
 
 bastille template sftp patater/sftp
 bastille template www patater/www
